08dcdf8b2af29d9f159e423bce734aace3db6988
sebastian cruz <jsebas.ct@gmail.com>
2017-02-18 12:35:55 -0300

    feat: read data from collection

--- find-pet/imports/ui/App.jsx
+++ find-pet/imports/ui/App.jsx
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data'
+
+import {Tasks} from '../api/tasks.js'
 
 import Task from './Task.jsx';
 
 // App component - represents the whole app
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is pet 1' },
-      { _id: 2, text: 'This is pet 2' },
-      { _id: 3, text: 'This is pet 3' },
-    ];
-  }
+class App extends Component {
+  // getTasks() {
+  //   return [
+  //     { _id: 1, text: 'This is pet 1' },
+  //     { _id: 2, text: 'This is pet 2' },
+  //     { _id: 3, text: 'This is pet 3' },
+  //   ];
+  // }
 
   renderTasks() {
-    return this.getTasks().map((task) => (
+    //return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -32,3 +36,16 @@ export default class App extends Component {
     );
   }
 }
+
+App.propTypes  = {
+  tasks: PropTypes.array.isRequired,
+};
+
+
+export default createContainer(()=>{
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+}, App);
+
+
